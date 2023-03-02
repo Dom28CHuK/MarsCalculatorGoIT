70b575ab90eed7ebb308eebffb98868b17f941c2
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-02 12:49:24 +0200

    add boolean canPrincessMarry(int day)

--- project/ProfessorHelper.java
+++ project/ProfessorHelper.java
@@ -8,12 +8,17 @@ public class ProfessorHelper {
     public boolean areNumbersFatal(int first, int second) {
         return first * 5 + 3 == second;
     }
+    public boolean canPrincessMarry(int day) {
+        return day > 15;
+    }
 
     //Test output
     public static void main(String[] args) {
         ProfessorHelper helper = new ProfessorHelper();
-        System.out.println("areNumbersCool(3, 3) = " + helper.areNumbersCool(3, 3)); //Should be true
-        System.out.println("areNumbersFatal(1, 8) = " + helper.areNumbersFatal(1, 8)); //Should be true
-        System.out.println("areNumbersFatal(1, 9) = " + helper.areNumbersFatal(1, 9));
+        //System.out.println("areNumbersCool(3, 3) = " + helper.areNumbersCool(3, 3)); //Should be true
+        //System.out.println("areNumbersFatal(1, 8) = " + helper.areNumbersFatal(1, 8)); //Should be true
+        System.out.println("canPrincessMarry(20) = " + helper.canPrincessMarry(20)); //Should be true
+        System.out.println("canPrincessMarry(20) = " + helper.canPrincessMarry(16));
+        System.out.println("canPrincessMarry(20) = " + helper.canPrincessMarry(4));
     }
 }
