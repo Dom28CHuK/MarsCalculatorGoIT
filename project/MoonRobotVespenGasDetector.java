20e5e8a0d647916d31add8ab703d25aa3343a511
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-03 13:09:45 +0200

    update isDensityOk

--- project/MoonRobotVespenGasDetector.java
+++ project/MoonRobotVespenGasDetector.java
@@ -6,7 +6,8 @@ public class MoonRobotVespenGasDetector {
     }
 
     public boolean isDensityOk(int density) {
-        return true;
+        return (density >= 1000 && density <= 5000) ^
+                (density >= 10000 && density <= 15000);
     }
 
     public boolean isTemperatureOk(boolean cold) {
