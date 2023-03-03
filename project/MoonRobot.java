6be35d6a17d5a75bb48d19d0031730620b6077bc
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-03 11:05:34 +0200

    Update MoonRobot

--- project/MoonRobot.java
+++ project/MoonRobot.java
@@ -12,18 +12,16 @@ public class MoonRobot {
         return isEngineWorking ^ isRobotStanding;
     }
 
+    public boolean shouldSleep(boolean isDayNow) {
+        return isDayNow == false;
+    }
+
     //Test output
     public static void main(String[] args) {
         MoonRobot robot = new MoonRobot();
 
-        //Should be false
-        System.out.println("canOvercomeHole(50, 60) = " + robot.canOvercomeHole(50, 60));
-        System.out.println("canOvercomeHole(100, 80) = " + robot.canOvercomeHole(100, 80));
-
-        System.out.println("canOvercomeHole(100, 90) = " + robot.canOvercomeHole(100, 90));
-        System.out.println("canJumpOverHill(180, 100) = " + robot.canJumpOverHill(180, 100));
-
-        System.out.println("areSensorsOk(true, true) = " + robot.areSensorsOk(true, true));
-        System.out.println("areSensorsOk(false, true) = " + robot.areSensorsOk(false, true));
+        System.out.println("shouldSleep(true) = " + robot.shouldSleep(true));
+        System.out.println("shouldSleep(false) = " + robot.shouldSleep(false));
     }
 }
+
