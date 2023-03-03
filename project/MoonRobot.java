dedd4c84bfe6b311603ebfa89463d78ae2cc6194
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-03 08:00:03 +0200

    Update MoonRobot
    Test

--- project/MoonRobot.java
+++ project/MoonRobot.java
@@ -5,6 +5,10 @@ public class MoonRobot {
         return holeDepth < 34 || batteryPercent > 67;
     }
 
+    public boolean canJumpOverHill(int hillHeight, int batteryPercent) {
+        return hillHeight <= 150 && batteryPercent >= 90;
+    }
+
     //Test output
     public static void main(String[] args) {
         MoonRobot robot = new MoonRobot();
@@ -12,5 +16,8 @@ public class MoonRobot {
         //Should be false
         System.out.println("canOvercomeHole(50, 60) = " + robot.canOvercomeHole(50, 60));
         System.out.println("canOvercomeHole(100, 80) = " + robot.canOvercomeHole(100, 80));
+
+        System.out.println("canOvercomeHole(100, 90) = " + robot.canOvercomeHole(100, 90));
+        System.out.println("canJumpOverHill(180, 100) = " + robot.canJumpOverHill(180, 100));
     }
 }
