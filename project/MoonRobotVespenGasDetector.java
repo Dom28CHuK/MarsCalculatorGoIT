7aca84adcb56764a5a7e7fcbebcdebeaa2d0d527
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-03 12:42:29 +0200

    add some methods

--- project/MoonRobotVespenGasDetector.java
+++ project/MoonRobotVespenGasDetector.java
@@ -2,6 +2,28 @@ package project;
 
 public class MoonRobotVespenGasDetector {
     public boolean isAcidOk(int acidValue, int minAcid, int maxAcid) {
-        
+        return true;
+    }
+
+    public boolean isDensityOk(int density) {
+        return true;
+    }
+
+    public boolean isTemperatureOk(boolean cold) {
+        return true;
+    }
+
+    public boolean isGroundOkForVespenGas(int acidValue, int minAcid, int maxAcid, int density, boolean cold) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        MoonRobotVespenGasDetector vespen = new MoonRobotVespenGasDetector();
+
+        System.out.println(vespen.isAcidOk(100, 50, 150)); //true
+        System.out.println(vespen.isDensityOk(1500)); //true
+        System.out.println(vespen.isDensityOk(30000)); //false
+        System.out.println(vespen.isTemperatureOk(false)); //true
+        System.out.println(vespen.isGroundOkForVespenGas(120, 75, 150, 7500, false)); // true
     }
 }
