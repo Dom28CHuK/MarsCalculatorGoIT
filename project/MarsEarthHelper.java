1f1425fdb931b2893d2434a8c9575ee2a57ebab8
Andrii Domchuk <andriy28domchuk@gmail.com>
2023-03-04 09:43:40 +0200

    Update
    Add method public byte countAppleCount(int garden1Count, int garden2Count)

--- project/MarsEarthHelper.java
+++ project/MarsEarthHelper.java
@@ -4,10 +4,19 @@ public class MarsEarthHelper {
     public int convertAppleCount(float count) {
         return (int) count;
     }
+
+    public byte countAppleCount(int garden1Count, int garden2Count) {
+        return (byte) (garden1Count + garden2Count);
+    }
     public static void main(String[] args) {
         MarsEarthHelper helper = new MarsEarthHelper();
 
         //Should be 10
         System.out.println("convertAppleCount(10f) = " + helper.convertAppleCount(11.5f));
+        //Should be 17
+        System.out.println("countAppleCount(10, 7) = " + helper.countAppleCount(10, 7));
+
+        System.out.println("countAppleCount(10, 30) = " + helper.countAppleCount(10, 30));
+        System.out.println("countAppleCount(50, 1) = " + helper.countAppleCount(50, 1));
     }
 }
